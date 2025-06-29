3a2472d905176c2d9cefbdc78cfefb48935fb797
Emanuel Alcala-Arredondo <alcala.arredondo1@gmail.com>
2025-06-28 19:04:11 -0700

    Added full CRUD funtionality. Now able to update employee information, employee hours and able to delete it as well.

--- Components/Services/EmployeeService.cs
+++ Components/Services/EmployeeService.cs
@@ -31,6 +31,55 @@ public class EmployeeService
             await _context.SaveChangesAsync();
         }
     }
+
+    public async Task ModifyEmployeeAsync(Employee employee)
+    {
+        Employee existing = await _context.Employees.FindAsync(employee.EmployeeId);
+
+        if (existing is not null)
+        {
+            existing.FirstName = employee.FirstName;
+            existing.LastName = employee.LastName;
+            existing.Address = employee.Address;
+            existing.Payrate = employee.Payrate;
+            existing.Hours = employee.Hours;
+
+            await _context.SaveChangesAsync();
+        }
+    }
+
+    public async Task DeleteEmployeeAsync(Employee employee)
+    {
+        Employee existing = await _context.Employees.FindAsync(employee.EmployeeId);
+
+        if (existing is not null)
+        {
+            _context.Employees.Remove(existing);
+            await _context.SaveChangesAsync();
+        }
+    }
+
+    public async Task ModifyHoursAsync(Employee employee)
+    {
+        Employee existing = await _context.Employees.FindAsync(employee.EmployeeId);
+
+        if (existing is not null)
+        {
+            existing.Hours = employee.Hours;
+            await _context.SaveChangesAsync();
+        }
+    }
+
+    public async Task ZeroizeHoursAsync(Employee employee)
+    {
+        Employee existing = await _context.Employees.FindAsync(employee.EmployeeId);
+
+        if (existing is not null)
+        {
+            existing.Hours = 0.0;
+            await _context.SaveChangesAsync();
+        }
+    }
 }
 public class NewHours
 {
